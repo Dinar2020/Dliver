a5bc370c0532a72e050b69f6d8631cd8c3a3d0d4
Dinar2020 <dinario@outlook.com.br>
2021-04-25 11:29:49 -0300

    criação de CadastroRestaurantes,CategoriasRelacão no models.py, url no admin.py e alterações feitas no migrations referente à alterações feitas no models.py - dinario

--- vendas/admin.py
+++ vendas/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Venda, Produto, Cliente, CadastroAtendente, CadastroLocalDeEntrega
+from .models import Venda, Produto, Cliente, CadastroAtendente, CadastroLocalDeEntrega, CadastroRestaurante,\
+    ClientePegueLeve, Bebida, Hamburgue, Pizza, Passaporte, Pastel, CategoriasRelacao
 
 # Register your models here.
 
@@ -9,3 +10,11 @@ admin.site.register(Produto)
 admin.site.register(Cliente)
 admin.site.register(CadastroAtendente)
 admin.site.register(CadastroLocalDeEntrega)
+admin.site.register(CadastroRestaurante)
+admin.site.register(ClientePegueLeve)
+admin.site.register(Bebida)
+admin.site.register(Hamburgue)
+admin.site.register(Pizza)
+admin.site.register(Passaporte)
+admin.site.register(Pastel)
+admin.site.register(CategoriasRelacao)
