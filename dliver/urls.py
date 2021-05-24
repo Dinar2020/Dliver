f0b20beeece03b9ec67b8d4961ec6286acdd38b6
Vyytoor <givanildovitor104@gmail.com>
2021-05-24 16:56:38 -0300

    Adicionando Createview, Listview e Updateview para a classe Produto

--- dliver/urls.py
+++ dliver/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from .views import VendaCreateView, VendaListView, VendaUpdateView
+from .views import VendaCreateView, VendaListView, VendaUpdateView, ProdutoCreateView, ProdutoUpdateView, ProdutoListView
 
 urlpatterns = [
     path('cadastrar/venda', VendaCreateView.as_view(), name="cadastrar_venda"),
     path('listar/venda', VendaListView.as_view(), name="listar_venda"),
     path('atualizar/venda/<int:pk>', VendaUpdateView.as_view(), name="atualizar_venda"),
+
+    path('cadastrar/produto', ProdutoCreateView.as_view(), name="cadastrar_produto"),
+    path('listar/produto', ProdutoListView.as_view(), name="listar_produto"),
+    path('atualizar/produto', ProdutoUpdateView.as_view(), name="atualizar_produto"),
+
 ]
 
--- dliver/views.py
+++ dliver/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView, UpdateView
-from .models import Venda
+from .models import Venda, Produto
 from django.urls import reverse_lazy
 
 # Create your views here.
 
+######################## VENDA ####################################
 
 class VendaCreateView(CreateView):
     model = Venda
@@ -14,18 +15,6 @@ class VendaCreateView(CreateView):
 
     def get_success_url(self):
         return reverse_lazy("listar_venda")
-
-
-# class ProdutoCreateView(CreateView):
-#     model = Produto
-#     template_name = 'cadastrar/produto.html'
-#
-#     # Caso eu queira que sejam cadastrados todos os fields do meu Model você colocará o fields = '__all__'
-#     # Caso queira especificar, colocar da forma abaixo. Por exemplo, se quiser apenas nome colocar fields = ['nome']
-#     fields = ['nome', 'valor']
-#
-#     def get_success_url(self):
-#         return reverse_lazy('cadastrar_produto') FAZER A CREATEVIEW DE
 
 
 class VendaListView(ListView):
@@ -43,3 +32,35 @@ class VendaUpdateView(UpdateView):
     def get_success_url(self):
         return reverse_lazy('listar_venda')
 
+######################## VENDA ####################################
+
+
+####################### PRODUTO ###################################
+
+class ProdutoCreateView(CreateView):
+    model = Produto
+    template_name = 'cadastrar/produto.html'
+    # Caso eu queira que sejam cadastrados todos os fields do meu Model você colocará o fields = '__all__'
+    # Caso queira especificar, colocar da forma abaixo. Por exemplo, se quiser apenas nome colocar fields = ['nome']
+    fields = ['nome', 'valor']
+
+    def get_success_url(self):
+        return reverse_lazy('cadastrar_produto')
+
+
+class ProdutoListView(ListView):
+    model = Produto
+    template_name = 'listar/produto.html'
+    paginate_by = 5
+
+
+class ProdutoUpdateView(UpdateView):
+    model = Produto
+    template_name = 'atualizar/produto.html'
+
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('listar_produto')
+
+####################### PRODUTO ###################################
