5cce8ab69c7949f8602cba1c7ae2bd5e58b9b743
Dinar2020 <dinario@outlook.com.br>
2021-05-25 22:08:10 -0300

    criadas as views para os clientes pegueleve e normais , fornecedor , entrega_produto, produto, venda. Juntamente com suas respectivas rotas e templates : atualizar, cadastrar e listar.

--- dliver/urls.py
+++ dliver/urls.py
@@ -1,10 +1,37 @@
 from django.contrib import admin
 from django.urls import path
-from .views import VendaCreateView, VendaListView, VendaUpdateView
+from .views import VendaCreateView, VendaListView, VendaUpdateView, ProdutoCreateView,\
+    ProdutoUpdateView, ProdutoListView, EntregaProdutoCreateView, EntregaProdutoListView, EntregaProdutoUpdateView,\
+    ClienteCreateView, ClienteListView, ClienteUpdateView, ClientePegueLeveCreateView, ClientePegueLeveListView,\
+    ClientePegueLeveUpdateView, CadastroFornecedorCreateView, CadastroFornecedorListView, CadastroFornecedorUpdateView,\
+    FormasPagamentoCreateView, FormasPagamentoListView, FormasPagamentoUpdateView,CadastroAtendenteCreateView
 
 urlpatterns = [
     path('cadastrar/venda', VendaCreateView.as_view(), name="cadastrar_venda"),
     path('listar/venda', VendaListView.as_view(), name="listar_venda"),
     path('atualizar/venda/<int:pk>', VendaUpdateView.as_view(), name="atualizar_venda"),
-]
 
+    path('cadastrar/produto', ProdutoCreateView.as_view(), name="cadastrar_produto"),
+    path('listar/produto', ProdutoListView.as_view(), name="listar_produto"),
+    path('atualizar/produto/<int:pk>', ProdutoUpdateView.as_view(), name="atualizar_produto"),
+
+    path('cadastrar/entregaproduto', EntregaProdutoCreateView.as_view(), name="cadastrar_entrega_produto"),
+    path('listar/entregaproduto', EntregaProdutoListView.as_view(), name="listar_entrega_produto"),
+    path('atualizar/entregaproduto/<int:pk>', EntregaProdutoUpdateView.as_view(), name="atualizar_entrega_produto"),
+
+    path('cadastrar/cliente', ClienteCreateView.as_view(), name="cadastrar_cliente"),
+    path('listar/cliente', ClienteListView.as_view(), name="listar_entrega_cliente"),
+    path('atualizar/cliente/<int:pk>', ClienteUpdateView.as_view(), name="atualizar_cliente"),
+
+    path('cadastrar/clientepegueleve', ClientePegueLeveCreateView.as_view(), name="cadastrar_clientepegueleve"),
+    path('listar/clientepegueleve', ClientePegueLeveListView.as_view(), name="listar_entrega_clientepegueleve"),
+    path('atualizar/clientepegueleve/<int:pk>', ClientePegueLeveUpdateView.as_view(), name="atualizar_clientepegueleve"),
+
+    path('cadastrar/fornecedor', CadastroFornecedorCreateView.as_view(), name="cadastrar_fornecedor"),
+    path('listar/fornecedor', CadastroFornecedorListView.as_view(), name="listar_fornecedor"),
+    path('atualizar/fornecedor/<int:pk>', CadastroFornecedorUpdateView.as_view(), name="atualizar_fornecedor"),
+
+    path('cadastrar/formaspagamento', FormasPagamentoCreateView.as_view(), name="cadastrar_formas_pagamento"),
+    path('listar/formaspagamento', FormasPagamentoListView(), name="listar_formas_pagamento"),
+    path('atualizar/formaspagamento/<int:pk>', FormasPagamentoUpdateView.as_view(), name="atualizar_formas_pagamento"),
+]
--- dliver/views.py
+++ dliver/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView, UpdateView
-from .models import Venda
+from .models import Venda, Produto, EntregaProduto, Cliente, ClientePegueLeve, CadastroFornecedor, FormasPagamento, \
+    CadastroAtendente, CadastroLocalDeEntrega, CadastroRestaurante, FazerPedido
 from django.urls import reverse_lazy
 
 # Create your views here.
 
+######################## VENDA ####################################
 
 class VendaCreateView(CreateView):
     model = Venda
@@ -12,21 +14,8 @@ class VendaCreateView(CreateView):
 
     fields = '__all__'
 
-
     def get_success_url(self):
         return reverse_lazy("listar_venda")
-
-
-# class ProdutoCreateView(CreateView):
-#     model = Produto
-#     template_name = 'cadastrar/produto.html'
-#
-#     # Caso eu queira que sejam cadastrados todos os fields do meu Model você colocará o fields = '__all__'
-#     # Caso queira especificar, colocar da forma abaixo. Por exemplo, se quiser apenas nome colocar fields = ['nome']
-#     fields = ['nome', 'valor']
-#
-#     def get_success_url(self):
-#         return reverse_lazy('cadastrar_produto') FAZER A CREATEVIEW DE
 
 
 class VendaListView(ListView):
@@ -43,4 +32,293 @@ class VendaUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse_lazy('listar_venda')
+
+######################## VENDA ####################################
+
+
+####################### PRODUTO ###################################
+
+class ProdutoCreateView(CreateView):
+    model = Produto
+    template_name = 'cadastrar/produto.html'
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('cadastrar_produto')
+
+
+class ProdutoListView(ListView):
+    model = Produto
+    template_name = 'listar/produto.html'
+    paginate_by = 5
+
+
+class ProdutoUpdateView(UpdateView):
+    model = Produto
+    template_name = 'atualizar/produto.html'
+
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('listar_produto')
+
+####################### PRODUTO ###################################
+
+####################### ENTREGA PRODUTO ###################################
+
+class EntregaProdutoCreateView(CreateView):
+    model = EntregaProduto
+    template_name = 'cadastrar/entrega_produto.html'
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('cadastrar_entrega_produto')
+
+
+class EntregaProdutoListView(ListView):
+    model = EntregaProduto
+    template_name = 'listar/entrega_produto.html'
+    paginate_by = 5
+
+
+class EntregaProdutoUpdateView(UpdateView):
+    model = EntregaProduto
+    template_name = 'atualizar/entrega_produto.html'
+
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('listar_entrega_produto')
+
+####################### ENTREGA PRODUTO ###################################
+
+####################### CLIENTE ###################################
+
+class ClienteCreateView(CreateView):
+    model = Cliente
+    template_name = 'cadastrar/entrega_cliente.html'
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('cadastrar_cliente')
+
+
+class ClienteListView(ListView):
+    model = Cliente
+    template_name = 'listar/cliente.html'
+    paginate_by = 5
+
+
+class  ClienteUpdateView(UpdateView):
+    model = Cliente
+    template_name = 'atualizar/cliente.html'
+
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('listar_cliente')
+
+####################### CLIENTE ###################################
+
+####################### CLIENTE PEGUE-LEVE  ###################################
+
+class ClientePegueLeveCreateView(CreateView):
+    model = ClientePegueLeve
+    template_name = 'cadastrar/clientepegueleve.html'
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('cadastrar_clientepegueleve')
+
+
+class ClientePegueLeveListView(ListView):
+    model = ClientePegueLeve
+    template_name = 'listar/clientepegueleve.html'
+    paginate_by = 5
+
+
+class ClientePegueLeveUpdateView(UpdateView):
+    model = ClientePegueLeve
+    template_name = 'atualizar/clientepegueleve.html'
+
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('listar_clientepegueleve')
+
+####################### CLIENTE PEGUE-LEVE ###################################
+
+####################### FORNECEDOR  ###################################
+
+class CadastroFornecedorCreateView(CreateView):
+    model = CadastroFornecedor
+    template_name = 'cadastrar/fornecedor.html'
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('cadastrar_fornecedor')
+
+
+class CadastroFornecedorListView(ListView):
+    model = CadastroFornecedor
+    template_name = 'listar/fornecedor.html'
+    paginate_by = 5
+
+
+class CadastroFornecedorUpdateView(UpdateView):
+    model = CadastroFornecedor
+    template_name = 'atualizar/fornecedor.html'
+
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('listar_fornecedor')
+
+####################### FORNECEDOR ###################################
+
+######################## FormasPagamento ####################################
+
+class FormasPagamentoCreateView(CreateView):
+    model = FormasPagamento
+    template_name = 'cadastrar/venda.html'
+
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy("listar_venda")
+
+
+class FormasPagamentoListView(ListView):
+    model = FormasPagamento
+    template_name = 'listar/venda.html'
+    paginate_by = 5
+
+
+class FormasPagamentoUpdateView(UpdateView):
+    model = FormasPagamento
+    template_name = 'atualizar/venda.html'
+
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('listar_venda')
+
+######################## FormasPagamento ####################################
+
+######################## CadastroAtendente ####################################
+
+class CadastroAtendenteCreateView(CreateView):
+    model = CadastroAtendente
+    template_name = 'cadastrar/venda.html'
+
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy("listar_venda")
+
+
+class CadastroAtendenteListView(ListView):
+    model = CadastroAtendente
+    template_name = 'listar/venda.html'
+    paginate_by = 5
+
+
+class CadastroAtendenteUpdateView(UpdateView):
+    model = CadastroAtendente
+    template_name = 'atualizar/venda.html'
+
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('listar_venda')
+
+######################## CadastroAtendente ####################################
+
+
+######################## CadastroLocalDeEntrega ####################################
+
+class CadastroLocalDeEntregaCreateView(CreateView):
+    model = CadastroLocalDeEntrega
+    template_name = 'cadastrar/venda.html'
+
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy("listar_venda")
+
+
+class CadastroLocalDeEntregaListView(ListView):
+    model = CadastroLocalDeEntrega
+    template_name = 'listar/venda.html'
+    paginate_by = 5
+
+
+class CadastroLocalDeEntregaUpdateView(UpdateView):
+    model = CadastroLocalDeEntrega
+    template_name = 'atualizar/venda.html'
+
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('listar_venda')
+
+######################## CadastroLocalDeEntrega ####################################
+
+######################## CadastroRestaurante ####################################
+
+class CadastroRestauranteCreateView(CreateView):
+    model = CadastroRestaurante
+    template_name = 'cadastrar/venda.html'
+
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy("listar_venda")
+
+
+class CadastroRestauranteListView(ListView):
+    model = CadastroRestaurante
+    template_name = 'listar/venda.html'
+    paginate_by = 5
+
+
+class CadastroRestauranteUpdateView(UpdateView):
+    model = CadastroRestaurante
+    template_name = 'atualizar/venda.html'
+
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('listar_venda')
+
+######################## CadastroRestaurante ####################################
+
+######################## FazerPedido ####################################
+
+class FazerPedidoCreateView(CreateView):
+    model = FazerPedido
+    template_name = 'cadastrar/venda.html'
+
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy("listar_venda")
+
+
+class FazerPedidoListView(ListView):
+    model = FazerPedido
+    template_name = 'listar/venda.html'
+    paginate_by = 5
+
+
+class FazerPedidoUpdateView(UpdateView):
+    model = FazerPedido
+    template_name = 'atualizar/venda.html'
+
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('listar_venda')
+
+######################## FazerPedido ####################################
 
